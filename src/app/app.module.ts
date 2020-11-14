13346c387772a9bd2e9f8c4e893420033df9ea0e
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2020-11-14 16:28:39 +0300

    add a component navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubComponent } from './github/github.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GithubComponent
+    GithubComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
