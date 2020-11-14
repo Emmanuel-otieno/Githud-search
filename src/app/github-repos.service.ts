01b93a912622589c44bf7d1ae24219f0386c1e11
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2020-11-14 21:01:36 +0300

    import my class and git my objects value

--- src/app/github-repos.service.ts
+++ src/app/github-repos.service.ts
@@ -1,10 +1,21 @@
 import { Injectable } from '@angular/core';
+import { User } from './user';
+import { Repository } from './repository';
+import { HttpClient} from '@angular/common/http';
+import { environment} from '../environments/environment'
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubReposService {
+
+  user:User;
+  repo:Repository;
   
 
-  constructor() { }
+  constructor(private http:HttpClient) {
+
+    
+
+   }
 }
