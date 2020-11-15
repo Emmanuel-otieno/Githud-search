5e37c035da29b3ba89b8716ee6c16978b889d5a7
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2020-11-15 17:11:23 +0300

    add a promise function to my sevice

--- src/app/github-repos.service.ts
+++ src/app/github-repos.service.ts
@@ -15,4 +15,61 @@ export class GithubReposService {
 
   constructor(private http:HttpClient) {
 
+    this.user = new User("","","","","","","","",new Date())
+    this.repo = new Repository("","","","","")   
+
+   }
+
+   findUser(searchName:string){
+    interface ApiResponse{
+       public_repos:any,
+       login:any,
+       html_url:any,
+       name:any,
+       avatar_url: any,
+       followers:any,
+       following:any,
+       repos_url:any,
+       created_at:Date,
+}
+let promise = new Promise((resolve, reject) => {
+  let apiURL = 'https://api.github.com/users/' + searchName + '?access_token=' + environment.apikey;
+  this.http.get<ApiResponse>(apiURL)
+    .toPromise()  
+    .then(
+      res => { // Success
+        this.user = res;
+        resolve();
+      },
+      (error) =>{
+        reject();
+      }
+    );
+});
+return promise;
+
+getRepo(searchName:string){
+  interface ApiResponse{
+    html_url:string,
+    name:string,
+    repos_url:string,
+    description:string,
+    forks_count:number,
   }
+  let promise = new Promise((resolve, reject) => {
+    let apiURL = 'https://api.github.com/users/' + searchName + '/repos?access_token=' + environment.apikey;
+    this.http.get<ApiResponse>(apiURL)
+      .toPromise()
+      .then(
+        res => { // Success
+          this.repo = res;
+          resolve();
+        },
+        ()=>{
+          reject();
+        }
+      );
+  });
+  return promise;
+}
+}                   
