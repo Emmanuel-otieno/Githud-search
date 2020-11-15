6e06e972d50abb4af4f89e9f0a0f31e3e771bf62
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2020-11-15 21:03:16 +0300

    add a constructor to repository class

--- src/app/repository.ts
+++ src/app/repository.ts
@@ -1,2 +1,9 @@
 export class Repository {
+    constructor(
+        public html_url:any,
+        public name:any,
+        public repos_url:any,
+        public description:any,
+        public forks_count:any,
+    ){}
 }
